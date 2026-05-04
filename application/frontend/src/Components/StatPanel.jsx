fc15b471e7a065c913dccd4f7c50a936573d5f74
caca <juliavuong378@gmail.com>
2026-05-05 01:03:38 +1000

    Update Stat panels

--- application/frontend/src/Components/StatPanel.jsx
+++ application/frontend/src/Components/StatPanel.jsx
@@ -3,21 +3,27 @@ import styled from "styled-components";
 const Panel = styled.div`
   margin-top: 10px;
   padding: 12px;
-  border: 1px solid #1c1c1c;
+  border: 1px solid #dddddd;
   background: #f3f3f3;
   border-radius: 8px;
   width: 720px;
+  color: black;
 `;
 
 export default function StatPanel({ data }) {
   if (!data) return null;
 
   return (
+    <>
+    <h1>Crowd Tracking Statistics</h1>
+
     <Panel>
       <p><b>People:</b> {data.people}</p>
       <p><b>Confidence:</b> {data.confidence}</p>
       <p><b>Density:</b> {data.density}</p>
       <p><b>Risk:</b> {data.risk}</p>
     </Panel>
+
+    </>
   );
 }
